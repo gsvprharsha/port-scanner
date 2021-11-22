e60d7c3be07e5cdb971fc7fb7399c9e214108550
Prharsha G <63843351+gsvprharsha@users.noreply.github.com>
2021-11-22 20:46:18 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+GREEN='\033[1;32m'
+NC='\033[0m'
+pip3 install sockets
+pip3 install IPy
+pip3 install colored
+pip3 install termcolor
+echo " "
+echo -e "${GREEN}[+]The dependencies have been successfully installed !! Have Fun using The Tool${NC}"
